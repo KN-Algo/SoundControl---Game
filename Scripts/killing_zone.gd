a766a9aec6c02a149bc51d248d9fe9914d7fc569
Igor Olewicz <94185939+CZIWELLO@users.noreply.github.com>
2025-02-12 15:36:55 +0100

    killing zone
    dodany killing zone

--- Scripts/killing_zone.gd
+++ Scripts/killing_zone.gd
@@ -1,17 +1,12 @@
 extends Area2D
 
 
-class_name Door
-
-@export var teleport: bool
 @export var destination_level_tag: String
 @export var destination_door_tag: String
-
-@onready var spawn = $Spawn
 
 
 func _on_body_entered(body):
 	if body is Player:
-		if teleport:
-			body.visible = false
+		body.visible = false
 		MenegerNawigacji.go_to_level(destination_level_tag, destination_door_tag)
+		
