8299f7751da86ed4a943504d7ab062e823e76ae2
Igor Olewicz <94185939+CZIWELLO@users.noreply.github.com>
2025-03-16 18:48:50 +0100

    merge ready
    wszystko działa jak ma działać

--- Scripts/timer.gd
+++ Scripts/timer.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var secs = $Panel/secs
 @onready var msecs = $Panel/msecs
 
-func _process(delta):
+func _process(_delta):
 	msecs.text = "%02d" % GlobaTimer.milisecnods
 	secs.text = "%02d:" % GlobaTimer.secnods
 	mins.text = "%02d:" % GlobaTimer.minutes
--- scripts/player.gd
+++ scripts/player.gd
@@ -17,7 +17,8 @@ func _on_spawn(pozycja: Vector2):
 func spawn_steam(dir: int, reposition: Vector2):
 	var steam = STEAM.instantiate()
 	get_tree().current_scene.add_child(steam)
-	
+	if dir == 0 and reposition != Vector2(0,30):
+		return
 	steam.global_position = global_position + reposition
 	steam.rotation_degrees = dir * 90 + 180
 
@@ -30,7 +31,7 @@ func _physics_process(delta: float) -> void:
 		spawn_steam(0,Vector2(0,30))
 		velocity.y = JUMP_VELOCITY
 		
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := int(Input.get_axis("ui_left", "ui_right"))
 	
 	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
 		spawn_steam(direction, Vector2(-20*direction,0))
