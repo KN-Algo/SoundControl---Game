2be6cb6797c3eb2f27b4bee4ebc891813e52569a
Igor Olewicz <94185939+CZIWELLO@users.noreply.github.com>
2025-04-05 18:03:02 +0200

    update
    update map i poczatek
    lvl 3

--- Scripts/LevelManager.gd
+++ Scripts/LevelManager.gd
@@ -4,7 +4,8 @@ var is_changing=false
 var level_dict={"mainmenu":"res://Scenes/main_menu.tscn",
 				"level0":"res://Scenes/world.tscn",
 				"level1":"res://Scenes/level_1.tscn",
-				"level2":"res://Scenes/level_2.tscn"}
+				"level2":"res://Scenes/level_2.tscn",
+				"level3":"res://Scenes/level_3.tscn"}
 var current_level="mainmenu"
 var entry=false
 func change_level(level_name):
